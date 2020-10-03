39bb3f18e2525730c191354da3ad39fb4b85929d
Anduri1 <noah@sieck.org>
2020-10-02 19:30:26 -0700

    Saving progress

--- scheduler/scheduler.cpp
+++ scheduler/scheduler.cpp
@@ -8,20 +8,28 @@
 #include "../includes/scheduler.h"
 
 
-void Scheduler::add(PCB p) {
-	//sort(); // Think this goes here? Does it call child func?
-	if (preemptive) {
-		ready_q->push(p);
+void Scheduler::add(PCB p) { // What is p here??
+	sort(); // Think this goes here? Does it call child func?
+
+	if(p.isEmpty()) {
+		PCB x;
+		ready_q->push(x);
 	} else {
-		//PCB np;
-		ready_q->push(p);
+
 	}
+
+	//if (preemptive) {
+		//ready_q->push(p);
+	//} else {
+		//PCB np;
+		//ready_q->push(p);
+	//}
 }
 
 PCB Scheduler::getNext() {
 	PCB x;
 	x = ready_q->front();
-	ready_q->pop();
+	//ready_q->pop();
 	return x; // New PCB object that equals first ready_q;
 }
 
@@ -31,11 +39,13 @@ bool Scheduler::isEmpty() {
 
 bool Scheduler::time_to_switch_processes(int tick_count, PCB &p) { // for preemptive modulo time slice by tick count if == 0, watch out for edge case where when first put on CPU it will also be 0
 	if (p.remaining_cpu_time == 0) {
+		getNext();
 		return true;
 	} else if (preemptive) { // time slice should = 0, which should be false, so !0 = true
 		if ((p.arrival_time == p.start_time)) {
 			return false;
 		} else if (time_slice % tick_count == 0) {
+			getNext();
 			return true;
 		}
 	}
